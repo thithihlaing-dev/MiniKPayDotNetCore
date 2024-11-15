524881c21236c673dcc2a8a53034b36764e9daa4
thithihlaing23 <thiritth23@gmail.com>
2024-11-15 21:37:23 +0630

    feat: Withdraw

---
+++ MiniKPayDotNetCore.CustomerMinimalApi/Controllers/Withdraw/WithdrawServiceEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using MiniKPayDotNetCore.Domain.Features.Customer;
+using MiniKPayDotNetCore.Domain.Features.Deposit;
+using MiniKPayDotNetCore.Domain.Features.Withdraw;
+using System.Runtime.CompilerServices;
+
+namespace MiniKPayDotNetCore.CustomerMinimalApi.Controllers.Withdraw
+{
+    public static class WithdrawServiceEndpoint
+    {
+        private static readonly WithdrawService _withdrawService = new WithdrawService();
+
+        public static void UseWithdrawServiceEndpoint(this IEndpointRouteBuilder app) {
+
+            app.MapPost("/withdraws/{moblieNo}/{balance}", (String mobileNo, Decimal amount) =>
+            {
+                var withdrawModel = _withdrawService.CreateWithdraw(mobileNo, amount);
+
+                return Results.Ok(withdrawModel);
+            })
+                   .WithName("CreateWithdraw")
+                   .WithOpenApi();
+        }
+    }
+}
---
+++ MiniKPayDotNetCore.Domain/Features/Withdraw/WithdrawService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniKPayDotNetCore.Database.Models;
+using MiniKPayDotNetCore.Domain.Features.CustomerBalance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniKPayDotNetCore.Domain.Features.Withdraw
+{
+    public class WithdrawService
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+        private static readonly CustomerBalanceService _customerBalanceService = new CustomerBalanceService();
+
+        public object CreateWithdraw(string mobileNo , decimal amount)
+        {
+            var customer = _db.TblCustomers.AsNoTracking().FirstOrDefault(x => x.CustomerMobileNo == mobileNo);
+            if (customer is null)
+            {
+                var message = new ResponseMessage
+                {
+                    responseMessage = "Invalid Mobile Number."
+                };
+                return message;
+            }
+            if (amount > 0)
+            {
+                var customerBalance = _customerBalanceService.WithdrawCustomerBalance(customer.CustomerId, amount);
+                if (customerBalance != null)
+                {
+                    var withdraw = new TblWithdraw
+                    {
+                        WithdrawMobileNo = mobileNo,
+                        WithdrawAmount = amount,
+                        DateTime = DateTime.Now
+                    };
+                    _db.TblWithdraws.Add(withdraw);
+                    _db.SaveChanges();
+                   
+                    return customerBalance;
+                }
+
+               
+            }
+
+            var error = new ResponseMessage
+            {
+                responseMessage = "Invalid Data"
+            };
+            return error;
+        }
+    }
+}
